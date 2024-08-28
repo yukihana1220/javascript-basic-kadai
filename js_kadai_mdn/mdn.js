e29a920b33f662dcbfb9df529fa7e2eaa263f20a
yukihana1220 <yukinko1220take@gmail.com>
2024-08-29 00:35:10 +0900

    kadai_mdnを削除

--- js_kadai_mdn/mdn.js
+++
@@ -1,7 +0,0 @@
-const year = new Date('August 22, 2023 23:15:30 GMT+11:00');
-const month = new Date('August 22, 2023 23:15:30 GMT+11:00');
-const date = new Date('August 22, 2023 23:15:30 GMT+11:00');
-
-console.log((year.getUTCFullYear() + '年')
-+ ((month.getUTCMonth()+1) + '月')
-+ (date.getUTCDate() + '日'));
